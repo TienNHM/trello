a8c76db1d7964d5a4e5f00f210217ed7937921a3
Nguyen Huynh Minh Tien <33385777+TienNHM@users.noreply.github.com>
2021-10-31 00:19:08 +0700

    design trello frontend

--- trello-web/src/App.js
+++ trello-web/src/App.js
@@ -1,9 +1,105 @@
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="hello-world">Hello world</h1>
+    <div className="trello-container">
+      <nav className="navbar app">App Bar</nav>
+      <nav className="navbar board">Board Bar</nav>
+      <div className="board-columns">
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://vietquality.vn/wp-content/uploads/2018/12/bs-e1544349690565.jpg" alt="Brainstorm" />
+              Title: TienNHM
+            </li>
+            <li>Số lượng tối ưu cho một nhóm chỉ nên ở mức 5-7 người</li>
+            <li>Tất cả mọi người đều có quyền lợi và nghĩa vụ đóng góp ý tưởng</li>
+            <li>Không được phép chỉ trích hoặc bác bỏ bất cứ ý tưởng nào trong quá trình này</li>
+            <li>Những ý tưởng có phần phá cách, mới lạ được khuyến khích</li>
+            <li>Số lượng tối ưu cho một nhóm chỉ nên ở mức 5-7 người</li>
+            <li>Tất cả mọi người đều có quyền lợi và nghĩa vụ đóng góp ý tưởng</li>
+            <li>Không được phép chỉ trích hoặc bác bỏ bất cứ ý tưởng nào trong quá trình này</li>
+            <li>Những ý tưởng có phần phá cách, mới lạ được khuyến khích</li>
+            </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://vietquality.vn/wp-content/uploads/2018/12/bs-e1544349690565.jpg" alt="Brainstorm" />
+              Title: TienNHM
+            </li>
+            <li>Số lượng tối ưu cho một nhóm chỉ nên ở mức 5-7 người</li>
+            <li>Tất cả mọi người đều có quyền lợi và nghĩa vụ đóng góp ý tưởng</li>
+            <li>Không được phép chỉ trích hoặc bác bỏ bất cứ ý tưởng nào trong quá trình này</li>
+            </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://vietquality.vn/wp-content/uploads/2018/12/bs-e1544349690565.jpg" alt="Brainstorm" />
+              Title: TienNHM
+            </li>
+            <li>Tất cả mọi người đều có quyền lợi và nghĩa vụ đóng góp ý tưởng</li>
+            <li>Không được phép chỉ trích hoặc bác bỏ bất cứ ý tưởng nào trong quá trình này</li>
+            </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://vietquality.vn/wp-content/uploads/2018/12/bs-e1544349690565.jpg" alt="Brainstorm" />
+              Title: TienNHM
+            </li>
+            <li>Số lượng tối ưu cho một nhóm chỉ nên ở mức 5-7 người</li>
+            <li>Tất cả mọi người đều có quyền lợi và nghĩa vụ đóng góp ý tưởng</li>
+            <li>Không được phép chỉ trích hoặc bác bỏ bất cứ ý tưởng nào trong quá trình này</li>
+            <li>Những ý tưởng có phần phá cách, mới lạ được khuyến khích</li>
+            <li>Số lượng tối ưu cho một nhóm chỉ nên ở mức 5-7 người</li>
+            <li>Tất cả mọi người đều có quyền lợi và nghĩa vụ đóng góp ý tưởng</li>
+            </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://vietquality.vn/wp-content/uploads/2018/12/bs-e1544349690565.jpg" alt="Brainstorm" />
+              Title: TienNHM
+            </li>
+            <li>Số lượng tối ưu cho một nhóm chỉ nên ở mức 5-7 người</li>
+            <li>Tất cả mọi người đều có quyền lợi và nghĩa vụ đóng góp ý tưởng</li>
+            <li>Không được phép chỉ trích hoặc bác bỏ bất cứ ý tưởng nào trong quá trình này</li>
+            <li>Tất cả mọi người đều có quyền lợi và nghĩa vụ đóng góp ý tưởng</li>
+            <li>Không được phép chỉ trích hoặc bác bỏ bất cứ ý tưởng nào trong quá trình này</li>
+            <li>Những ý tưởng có phần phá cách, mới lạ được khuyến khích</li>
+            </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://vietquality.vn/wp-content/uploads/2018/12/bs-e1544349690565.jpg" alt="Brainstorm" />
+              Title: TienNHM
+            </li>
+            <li>Số lượng tối ưu cho một nhóm chỉ nên ở mức 5-7 người</li>
+            <li>Tất cả mọi người đều có quyền lợi và nghĩa vụ đóng góp ý tưởng</li>
+            <li>Không được phép chỉ trích hoặc bác bỏ bất cứ ý tưởng nào trong quá trình này</li>
+            <li>Những ý tưởng có phần phá cách, mới lạ được khuyến khích</li>
+            <li>Số lượng tối ưu cho một nhóm chỉ nên ở mức 5-7 người</li>
+            <li>Tất cả mọi người đều có quyền lợi và nghĩa vụ đóng góp ý tưởng</li>
+            <li>Không được phép chỉ trích hoặc bác bỏ bất cứ ý tưởng nào trong quá trình này</li>
+            <li>Những ý tưởng có phần phá cách, mới lạ được khuyến khích</li>
+            </ul>
+          <footer>Add another card</footer>
+        </div>
+      </div>
     </div>
   );
 }
